d344bac2efd77edde998918c76b542757f617246
Jay Yang <hjyang1@uci.edu>
2018-03-25 12:28:51 -0700

    Random Human Colors spawn at start

---
+++ Learning AI/Assets/DNA.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour {
+
+    //Genes for color that will be passed later
+    public float r;
+    public float g;
+    public float b;
+
+    bool dead = false;
+    public float timeToDead = 0;
+
+    SpriteRenderer sRenderer;
+    Collider2D sCollider;
+
+    private void OnMouseDown()
+    {
+        dead = true;
+        timeToDead = PopulationManager.elapsed;
+        Debug.Log("Dead At: " + timeToDead);
+        sRenderer.enabled = false;
+        sCollider.enabled = false;
+    }
+    private void Start()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+        sCollider = GetComponent<Collider2D>();
+        sRenderer.color = new Color(r, g, b);
+    }
+}
